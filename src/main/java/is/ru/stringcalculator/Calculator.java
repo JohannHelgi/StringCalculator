880286265dcc5243a8a99642f80ff7cf5dc73ea7
Jóhann Helgi Gunnarsson <johannhg16@ru.is>
2017-10-13 16:52:33 +0000

    code for negative refactored

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -6,25 +6,36 @@ public class Calculator {
           if (text.equals("")) {
               return 0;
           }
+          else if (text.contains("-")) {
+            String negative = checkNegative(text);
+            throw new IllegalArgumentException("Negatives not allowed: " + negative);
+
+          }
           else if (text.contains("\n") || text.contains(",")) {
-                String numbers[] = text.split("\n|,");
-                String negative = "";
-                for (String number : numbers) {
-                    if (number.contains("-")) {
-                        negative = negative + number +",";
-                    }
-                }
-
-                if (negative.endsWith(", ")) {
-                      negative = negative.substring(0, negative.length() - 2);
-                }
-
-                if (!negative.equals("")) {
-                    throw new IllegalArgumentException("Negatives not allowed: " + negative);
-                }
+                String numbers[] = splitString(text);
                 return addNumbers(numbers);
           }
           else return toInt(text);
+      }
+
+      public static String[] splitString(String text) {
+          String numbers[] = text.split("\n|,");
+          return numbers;
+      }
+
+      public static String checkNegative(String text) {
+          String numbers[] = splitString(text);
+          String negative = "";
+          for (String number : numbers) {
+              if (number.contains("-")) {
+                  negative = negative + number +",";
+              }
+          }
+          if (negative.endsWith(", ")) {
+                negative = negative.substring(0, negative.length() - 2);
+          }
+
+          return negative;
       }
 
       public static int toInt(String integer) {
